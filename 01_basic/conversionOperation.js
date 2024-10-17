54b57f081601a8ca012881a1dce6f57a2a27b0d6
vishal kaushlani <vishalkaushlani2000@gmail.com>
2024-10-17 05:21:07 +0000

    conversion and comparision

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,23 @@
+
+
+// console.log("1" + "2")
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1)
+
+let gameCounter = 100
+// gameCounter++
+console.log(gameCounter)
+console.log(gameCounter++)
+// console.log(++gameCounter)
