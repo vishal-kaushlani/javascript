918e9269a632e397cdc35cb4d0fbe7dba4fa61e6
vishal kaushlani <vishalkaushlani2000@gmail.com>
2024-10-18 05:27:30 +0000

    string methods

---
+++ 01_basic/string.js
@@ -0,0 +1,39 @@
+const name = "sachin"
+
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+const stringName = 'vishal'
+
+// console.log(stringName[0])
+
+const gameName = new String('rahul gupta')
+
+// console.log(gameName)
+// console.log(gameName[0])
+// console.log(gameName.length)
+// console.log(gameName.__proto__)
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0, 4)
+
+// console.log(newString);
+
+const anotherString = gameName.slice(-7,7)
+// console.log('anotherString : '+anotherString)
+
+
+const newStringOne = "    vishal    "
+// console.log(newStringOne)
+
+// console.log(newStringOne.trim())
+
+const url = "https://vishal.com/vishal%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('vishal'))
+
